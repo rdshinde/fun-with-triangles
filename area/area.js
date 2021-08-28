fe427514cbe06fb3498143df0ed5fe57d42796bb
Rishikesh Shinde <rdshinde2000@gmail.com>
2021-08-28 17:19:20 +0530

    feat:area done

---
+++ area/area.js
@@ -0,0 +1,22 @@
+const base = document.querySelector('#base');
+const height = document.querySelector('#height');
+
+const calculateBtn = document.querySelector('#calculateBtn');
+const resultDiv = document.querySelector('.result');
+
+function message(msg){
+    resultDiv.innerText = msg;
+}
+
+function calculateArea(){
+    baseValue = parseFloat(base.value);
+    heightValue = parseFloat(height.value);
+    if(baseValue > 0 && heightValue > 0){
+        const area = (baseValue*heightValue)/2;
+        message(`The area of triangle is ${area}.`);
+    }
+    else{
+        message(`Invalid input.`);
+    }
+}
+calculateBtn.addEventListener('click',calculateArea);
